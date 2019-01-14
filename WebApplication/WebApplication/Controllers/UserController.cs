ef672e5b48d5706298e933aef72bca5b6fb17c91
kay-9 <amkhairi@outlook.com>
2019-01-14 15:03:39 +0100

    create the database && "Sign-in" web form

---
+++ WebApplication/WebApplication/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add(int id=0)
+        {
+            User usermodel1 = new User();
+            return View(usermodel1);
+        }
+    }
+}
